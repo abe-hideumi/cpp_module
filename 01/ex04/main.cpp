99c12ef6743e3ae6ee76307ed993fd69065501f2
Hideumi Abe <habe@c1r3s8.42tokyo.jp>
2026-06-30 15:32:08 +0900

    01/ex04

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
 #include <fstream>
-#include "input.hpp"
+#include <string>
+#include <cstdlib>
+#include <iomanip>
 
-static std::string	ft_replace(std::string line, const std::string& s1, const std::string& s2) {
-	std::string	tmp = "";
-	size_t	pos = 0;
+static std::string FtReplace(const std::string& line, const std::string& s1, const std::string& s2) {
+	size_t	prevPos = 0;
+	std::string	tmp;
+	size_t	pos = line.find(s1);
 
-	while ((pos = line.find(s1, pos)) != std::string::npos) {
-		tmp += line.substr(0, pos) + s2;
-		pos += s1.length();
+	while (pos  != std::string::npos) {
+		tmp += line.substr(prevPos, pos - prevPos) + s2;
+		prevPos = pos + s1.length();
+		pos = line.find(s1, prevPos);
 	}
+	tmp += line.substr(prevPos);
 	return tmp;
 }
 
-int	main() {
-	Input	input;
-	std::cout << input.getFileName() << std::endl;
-	std::cout << input.getS1() << std::endl;
-	std::cout << input.getS2() << std::endl;
-
-	std::ifstream	file(input.getFileName());
-	if (!file.is_open()) {
-		std::cerr << "Error: Could not open file " << input.getFileName() << std::endl;
+int	main(int ac, char **av) {
+	if (ac != 4) {
+		std::cerr << "Usage: ./ex04 <fileName> <s1> <s2>" << std::endl;
 		return 1;
 	}
+	std::string fileName = av[1];
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+
+	std::ifstream	file(fileName.c_str());
+	if (!file.is_open()) {
+		std::cerr << "Error: Could not open file " << fileName << std::endl;
+		return 1;
+	}
+	if (s1.empty()) {
+		std::cerr << "Error: s1 cannot be empty" << std::endl;
+		return 1;
+	}
+	fileName += ".replace";
+	std::ofstream	outFile(fileName.c_str());
 	std::string	line;
 	while (getline(file, line)) {
-		line = ft_replace(line, input.getS1(), input.getS2());
-		std::cout << line << std::endl;
+		line = FtReplace(line, s1, s2);
+		outFile << line << std::endl;
 	}
 	file.close();
+	outFile.close();
 	return 0;
 }
