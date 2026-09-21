a8f5f6c4765cb6711b52100f631d3d337f799b7f
うみ <umi@habe-MacBook-Pro.local>
2026-09-21 22:29:28 +0900

    04-ex02: Animalを抽象クラス化
    - makeSound()を純粋仮想関数にし、Animalをインスタンス化不可にした
    - Animal.cppからmakeSound()の実装を削除
    - MakefileのNAMEをex02に変更
    - main.cppにインスタンス化できない例をコメントで追記

--- 04/ex02/main.cpp
+++ 04/ex02/main.cpp
@@ -5,6 +5,10 @@
 #include <iostream>
 
 int main() {
+    // Animal is abstract: the following lines must NOT compile.
+    // Animal animal;
+    // Animal* animal = new Animal();
+
     std::cout << "===== Subject test =====" << std::endl;
     {
         const Animal* j = new Dog();
